b478d47beb479ca69d4b2d79fc11d6fe67dfd4d3
lennartblom <lennart@blom.de>
2020-10-03 14:01:16 +0200

    Add basic iteration of first 20 follower data

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const axios = require('axios');
 
-axios.get('https://randomuser.me/api/')
+const bearerToken = process.env.TWITTER_BEARER_TOKEN;
+const twitterHandle = "lennartblom";
+const requestConfig = {'headers': {
+        'Authorization': `Bearer ${bearerToken}`
+    }
+}
+
+let twitterApiUrl = `https://api.twitter.com/1.1/followers/list.json?cursor=-1&screen_name=${twitterHandle}&skip_status=true&include_user_entities=false`;
+
+let screenNamesToScratch = [];
+
+axios.get(twitterApiUrl, requestConfig)
   .then(response => {
-    console.log(response);
+    for (const user of response.data.users) {
+        screenNamesToScratch.push(user.screen_name);
+    }
+
+    console.log(screenNamesToScratch);
   })
   .catch(error => {
     console.log(error);
