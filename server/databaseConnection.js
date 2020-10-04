7f7ef02d7e5cd81995ecdae9ad8d66a49841d104
lennartblom <lennart@blom.de>
2020-10-04 11:32:13 +0200

    Optimize database queries and two level twitter-scraping

--- server/databaseConnection.js
+++ server/databaseConnection.js
@@ -1,15 +1,17 @@
 const neo4j = require('neo4j-driver')
 
 const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'neo'));
-const session = driver.session()
 
-
-async function startDbConnectivity(username, followers) {
+async function startDbConnectivity(username, followers, createInitialUsername) {
+    const session = driver.session()
     try {
-        const resultUsernameCreation = await session.run(
-            'CREATE (a:Person {name: $name}) RETURN a',
-            {name: username}
-        )
+        if(createInitialUsername) {
+            const resultUsernameCreation = await session.run(
+                'CREATE (a:Person {name: $name}) RETURN a',
+                {name: username}
+            )
+        }
+
 
         for(const follower of followers) {
             const resultFollowerCreation = await session.run(
@@ -27,12 +29,9 @@ async function startDbConnectivity(username, followers) {
         }
 
     } finally {
-
+        session.close();
     }
 
-// on application exit:
-    await session.close();
-    await driver.close();
 }
 
 module.exports = startDbConnectivity;
--- server/scrapeTwitter.js
+++ server/scrapeTwitter.js
@@ -20,7 +20,8 @@ axios.get(twitterApiUrl, requestConfig)
             screenNamesToScratch.push(user.screen_name);
         }
 
-        startDbConnectivity(twitterHandle, screenNamesToScratch).then(r => {
+        console.log("Trying to write relations from @", twitterHandle, "to his/her follower", screenNamesToScratch);
+        startDbConnectivity(twitterHandle, screenNamesToScratch, true).then(r => {
 
             for (const follower of screenNamesToScratch) {
                 let followerApiUrl = `https://api.twitter.com/1.1/followers/list.json?cursor=-1&screen_name=${follower}&skip_status=true&include_user_entities=false`;
@@ -28,22 +29,26 @@ axios.get(twitterApiUrl, requestConfig)
 
                 axios.get(followerApiUrl, requestConfig)
                     .then(response => {
-                        for (const user of response.data.users) {
-                            screenNamesToScratch.push(user.screen_name);
-                        }
+                        console.log("collection followers from @", follower);
+                        let secondLevelScreenNames = [];
 
-                        startDbConnectivity(twitterHandle, screenNamesToScratch).then(r => {
-                            console.log("Scratch follower of follower finished.")
+                        for (const user of response.data.users) {
+                            secondLevelScreenNames.push(user.screen_name);
+                        }
+                        console.log("Trying to write relations from @", follower, "to his/her follower", secondLevelScreenNames);
+                        startDbConnectivity(follower, secondLevelScreenNames, false).then(r => {
+                            console.log("Scratch follower of @", follower, "finished")
                         });
 
                     })
                     .catch(error => {
                         console.log("Unable to request followers from @", follower);
+                        console.log(error);
                     });
             }
         });
 
     })
     .catch(error => {
-        console.log(error);
+        console.log("Initial twitter API request did not work out", error);
     });
